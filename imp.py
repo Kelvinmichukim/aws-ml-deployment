171e975c597bafafaa438798987b75dc93f9fece
Kelvinmichukim <kelvinmich98@gmail.com>
2023-08-29 14:07:13 +0300

    AWS ML

---
+++ imp.py
@@ -0,0 +1,20 @@
+# import libraries
+import boto3, re, sys, math, json, os, sagemaker, urllib.request
+from sagemaker import get_execution_role
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import Image
+from IPython.display import display
+from time import gmtime, strftime
+from sagemaker.serializers import CSVSerializer
+
+# Define IAM role
+role = get_execution_role()
+prefix = 'sagemaker/DEMO-xgboost-dm'
+my_region = boto3.session.Session().region_name # set the region of the instance
+
+# this line automatically looks for the XGBoost image URI and builds an XGBoost container.
+xgboost_container = sagemaker.image_uris.retrieve("xgboost", my_region, "latest")
+
+print("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + xgboost_container + " container for your SageMaker endpoint.")
